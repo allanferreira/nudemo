6b738134a96093d11efca5e16b42bb7ccbcf2dd3
Chinnon Santos <chinnonsantos@gmail.com>
2019-12-04 14:19:37 -0300

    Updating CI/CD scripts 🎲

--- scripts/flutter_test.sh
+++ scripts/flutter_test.sh
@@ -1,5 +1,5 @@
 set -e # abort CI if an error happens
-flutter packages get
+flutter packages pub get
 flutter format --set-exit-if-changed lib test test_driver
-flutter analyze --no-current-package lib test/
+flutter analyze --no-pub
 flutter test --no-pub --coverage
